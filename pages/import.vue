948dbc6d32a51064fde6bf2a15dc7f5d84927eaf
Maxr1998 <max.rumpf1998@gmail.com>
2022-11-30 16:29:33 +0100

    Improve import description

--- pages/import.vue
+++ pages/import.vue
@@ -4,10 +4,15 @@
       <v-card-title>Select your Spotify GDPR archive</v-card-title>
       <v-card-text>
         You can select the ZIP archive that you downloaded from Spotify here.
+        Both account data and extended streaming history are supported.
+        Use extended streaming history for best results.
         <br>
-        It's normally named something like <code>my-spotify-data.zip</code>.
+        The file is normally named something like <code>my-spotify-data.zip</code>.
         <br>
-        <i>All data is processed locally and will stay on this device. No information will be sent to the server.</i>
+        <i>
+          Note: All data is processed locally in your browser and won't leave this device.
+          No information is transmitted to the server.
+        </i>
       </v-card-text>
       <v-card-text>
         <ImportHistory @success="showStats"/>

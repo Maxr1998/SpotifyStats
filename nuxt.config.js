76d014aa342825d3c99ff2efcfe8cd1485c13bc8
Maxr1998 <max.rumpf1998@gmail.com>
2022-11-30 17:15:49 +0100

    Customize link color

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,12 +12,12 @@ export default {
     titleTemplate: '%s - SpotifyStats',
     title: 'SpotifyStats',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
@@ -51,6 +51,7 @@ export default {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
+          anchor: 'whitesmoke',
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
